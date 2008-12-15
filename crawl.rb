19d89794b1b9762107ffcd2294552ad8b9067c8e
James Conroy-Finn <jamesconroyfinn@gmail.com>
2008-12-15 15:33:54 +0000

    Updated crawl.rb
     - Still need to tweak Crawler

--- crawl.rb
+++ crawl.rb
@@ -2,46 +2,96 @@
 require "rubygems"
 require "hpricot"
 require "ftools"
+require "optparse"
 
 class Crawler
-  def initialize(site_uri='cms.alphasights-001.vm.brightbox.net',
-                 wget_path='/usr/local/bin/wget',
-                 git_path='/usr/local/git/bin/git')
-    @site_uri = site_uri
-    @wget_path = wget_path
-    @git_path = git_path
-    sanity_check
+  def initialize(sites=['cms.alphasights-001.vm.brightbox.net'], options={})
+    
+    parse(ARGV)
+    
+    sites ||= []
+    ARGV.each do |arg|
+      sites += [arg.sub(/^(http:|https:)\/\//, '')]
+    end
+    
+    @sites = sites
   end
   
+  def run
+    unless sanity_check
+      print_usage
+      exit
+    end
+    
+    wget_sites_to_github(@sites)
+  end
+  
+  private
   def sanity_check
-    if @wget_path == '' && %x(which wget).length == 0
-      raise "wget not in $PATH. Please specify path to wget manually.\n\nCrawler.new('site_uri', '/path/to/wget')"
-    end
-    if @git_path == '' && %x(which git).length == 0
-      raise "git not in $PATH. Please specify path to git manually.\n\nCrawler.new('site_uri', '/path/to/git')"
-    end
+    [@options[:wget_path], @options[:git_path]].each do |binary_path|
+      binary_name = File.basename(binary_path)
+      
+      unless File.exists?(binary_path)
+        $stderr.puts "\"#{binary_path}\" does not exist."
+        print "Would you like to search for \"#{binary_name}\" in $PATH? (y/N): "
+        
+        if $stdin.gets.downcase.chomp! == 'y'
+          ENV['PATH'].split(':').each do |search_path|
+            
+            all_results = []
+            
+            if File.exists?(File.join(search_path, binary_name))
+              all_results += [File.join(search_path, binary_name)]
+            end # if File.exists?(File.join...
+          end # ENV['PATH'].split
+          
+          if all_results.length == 0
+            $stderr.puts "Could not find \"#{binary_name}\" in $PATH. Exiting..."
+            return false
+          else
+            print "Found #{all_results.length}"
+            if $stdin.gets.downcase.chomp! == 'y'
+              @options[binary_name.to_sym] = found
+              puts "Using \"#{found}\" for \"#{binary_name}\""
+              return true
+            end
+          end
+          
+          return false
+        end # $stdin.gets
+        
+        $stderr.puts "Path to \"#{binary_name}\" (#{binary_path}) is invalid. Exiting..."
+        return false
+      end # unless File.exists?(binary_path)
+    end # [@options[:wget_path]...
+  end # sanity_check
+  
+  def wget_sites_to_github(site_uris=[])
+    site_uris.each { |s| @options[:site_uri] = s; wget(s) && git_push(s) }
   end
   
-  def wget_sites(site_uris=[])
-    site_uris.each { |s| wget(s) }
-  end
-  
-  def wget(s=@site_uri)
-    command = "#{@wget_path} -rk#{'q' unless $VERBOSE} '#{s}'"
-    $stdout.puts(command)
+  def wget(s)
+    command = "#{@options[:wget_path]} -rk#{'q' unless $VERBOSE} '#{s}'"
+    puts command
     system command
     fix_php_css
+  end
+  
+  def git_push(s=@options[:site_uri])
+    command = "#{@options[:git_path]} add #{s} && #{@options[:git_path]} commit -m 'Crawl as of #{Time.now.to_s}' && #{@options[:git_path]} push"
+    puts(command)
+    system command
   end
   
   def fix_php_css
     html_files = Array.new
     
-    Dir.glob("#{@site_uri}/**/*.*").each do |f|
+    Dir.glob("#{@options[:site_uri]}/**/*.*").each do |f|
        html_files += [File.expand_path(f)] if f =~ /.html/
     end
     
     html_files.each do |f|
-      $stdout.puts "Parsing #{f}"
+      puts "Parsing #{f}"
       
       doc = Hpricot(File.read(f))
       
@@ -51,24 +101,66 @@ class Crawler
         
         return true if new_link.nil? # Files do not match our pattern. Give up.
         
-        @new_href = "#{new_link[1]}#{new_link[4]}-#{new_link[5]}_#{new_link[6]}-#{new_link[7]}.css"
+        # @new_href = "#{new_link[1]}/css/#{new_link[4]}-#{new_link[5]}_#{new_link[6]}-#{new_link[7]}.css"
         
-        File.copy(File.join(File.dirname(f), link['href']), File.join(File.dirname(f), @new_href))
-        link.set_attribute("href", @new_href)
+        @full_path_to_css = File.join(File.dirname(f), new_link[1],
+                                      'css',
+                                      "#{new_link[4]}-#{new_link[5]}_#{new_link[6]}-#{new_link[7]}.css")
+        
+        File.copy(File.join(File.dirname(f), link['href']), @full_path_to_css)
+        link.set_attribute("href", @full_path_to_css)
       end
       
       File.open(f, 'w') {|f| f.write(doc) }
     end
   end
   
-  def git_push(s=@site_uri)
-    command = "#{@git_path} add #{s} && #{@git_path} commit -m 'Crawl as of #{Time.now.to_s}' && #{@git_path} push"
-    $stdout.puts(command)
-    system command
+  def parse(argv)
+    options = {:wget_path => '/usr/local/bin/wget',
+               :git_path => '/usr/local/bin/git'}
+
+    argv.options do |opts|
+      opts.banner = "Usage: #{File.basename($PROGRAM_NAME)} [OPTIONS] SITE_URI(s)"
+
+      opts.separator ""
+      opts.separator "Specific Options:"
+
+      opts.on( "-w", "--wget", String,
+               "Full path to the wget binary (Must be at least version 1.12)" ) do |opt|
+        options[:wget_path] = opts
+      end
+
+      opts.on( "-g", "--git", String,
+               "Full path to the git binary" ) do |opt|
+        options[:git_path] = opts
+      end
+
+      opts.separator "Common Options:"
+
+      opts.on( "-h", "--help",
+               "Show this message." ) do
+        puts opts
+        exit
+      end
+      
+      begin
+        opts.parse!
+        @options = options
+        @usage = opts
+      rescue
+        puts opts
+        exit
+      end
+    end
   end
   
+  def print_usage
+    $stderr.puts "", @usage
+    exit
+  end  
 end
 
-c = Crawler.new()
-c.wget
-c.git_push
+c = Crawler.new(ARGV)
+# c.wget
+# c.git_push
+# c.sanity_check

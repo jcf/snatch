5956820bbe17af0c686b8764ce4c15a4f1d4b617
James Conroy-Finn <james@logi.cl>
2010-04-26 14:04:40 +0100

    Use #to_html when writing clean document to file

--- lib/snatch/clean.rb
+++ lib/snatch/clean.rb
@@ -20,15 +20,7 @@ class Snatch
       CSS.update(@doc, @working_directory)
       HTML.update(@doc, @working_directory)
 
-      File.open(@file_name, 'w') { |f| f.write @doc.to_xhtml }
+      File.open(@file_name, 'w') { |f| f.write @doc.to_html }
     end
-
-	private
-
-	  # Sadly we can't use this as our XSLT won't output valid XHTML
-		# def clean_xhtml
-		# 	xsl_io = File.open(File.expand_path('../../../xsl/pretty_print.xsl', __FILE__))
-		# 	Nokogiri::XSLT(xsl_io).apply_to(@doc)
-		# end
   end
 end
